3a7f8300083ca88eb6cf7e7e1e249cadaad1f46c
logicallaw <logicallawbio@gmail.com>
2024-01-28 22:27:08 +0900

    update:24.01.28

---
+++ index.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    const body = document.querySelector('body');
+    const p = document.createElement('p');
+    p.textContent = 'Hello Github!';
+    body.appendChild(p);
+})
